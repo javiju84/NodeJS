aa4507c7ab9e51257967968d725849225f3509d5
Javier <javiju84@gmail.com>
2016-04-12 21:16:31 +0200

    guardado

--- hello.js
+++ hello.js
@@ -1,7 +1,8 @@
 var http = require("http");
 
 var manejador = function(solicitud, respuesta){
-console.log("Hola Mundo");
+	console.log("recibimos una nueva petición");
+	respuesta.end("Hola mundo")
 };
 
 http.createServer();
