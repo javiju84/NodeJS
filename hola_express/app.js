3124d1a9ab1a2c85b60ab7bec3fb286e70af1b84
Javier <javiju84@gmail.com>
2016-04-13 20:12:16 +0200

    Motores de vistas, JADE y variables

--- hola_express/app.js
+++ hola_express/app.js
@@ -2,8 +2,10 @@ var express = require("express");
 
 var app = express();
 
+app.set("view engine", "jade");
+
 app.get("/",function(req,res){
-	res.send("HOla mundo");
+	res.render("index",{hola: "Hola Javiju"});//pasar variables
 });
 
 app.listen(8080);
